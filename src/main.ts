395e14a89a4a45180909187e20a4af9103a8c320
Yuriy Ustushenko <yuriy.ustushenko@gmail.com>
2025-07-31 21:27:18 +0800

    remove mdifont

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { createVuetify } from "vuetify"
 import * as components from "vuetify/components"
 import * as directives from "vuetify/directives"
 import { mdi } from "vuetify/iconsets/mdi"
-import "@mdi/font/css/materialdesignicons.css"
 import "vuetify/styles"
 
 import App from "./App.vue"
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import { fileURLToPath, URL } from "node:url"
+import { compression } from 'vite-plugin-compression2'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    compression({
+      algorithm: 'gzip',
+      include: [/\.(js|mjs|json|css|html)$/i],
+      deleteOriginalAssets: false,
+    })
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
