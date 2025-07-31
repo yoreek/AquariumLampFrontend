8d822daacb48e35edea123ef2c1775113a13e60e
Yuriy Ustushenko <yuriy.ustushenko@gmail.com>
2025-07-31 18:55:10 +0800

    fix time format

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -211,16 +211,21 @@ const updateDateTime = () => {
     hour12: false
   }
 
-  currentDateTime.value = now.toLocaleString('en-US', {
+  const dateStr = now.toLocaleDateString('en-US', {
     weekday: 'long',
     year: 'numeric',
     month: 'long',
-    day: 'numeric',
+    day: 'numeric'
+  })
+
+  const timeStr = now.toLocaleTimeString('en-US', {
     hour: '2-digit',
     minute: '2-digit',
     second: '2-digit',
     ...format
   })
+
+  currentDateTime.value = `${dateStr}, ${timeStr}`
 }
 
 const getDisplayTime = (time24: string) => {
